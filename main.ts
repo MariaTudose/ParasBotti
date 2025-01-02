979560501d9335be1facfa2c71c95d1ff468fbb0
Maria Tudose <dev.tudose@gmail.com>
2025-01-02 20:33:06 +0200

    Call remind event handler on startup

--- main.ts
+++ main.ts
@@ -1,15 +1,26 @@
 import { token } from "./config.json";
 import { Client, Events, GatewayIntentBits } from "discord.js";
 import { events } from "./events";
+import remind from "./events/remind";
+
+const guildId = "1216943520145866783";
 
 export const client = new Client({
-  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildPresences],
+  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildPresences, GatewayIntentBits.GuildMembers],
 });
 
 client.login(token);
 
 client.once(Events.ClientReady, (readyClient) => {
   console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+
+  readyClient.guilds.fetch(guildId).then((guild) => {
+    guild.members.fetch({ withPresences: true }).then((members) => {
+      members.forEach((member) => {
+        member.presence && remind.execute(null, member.presence);
+      });
+    });
+  });
 });
 
 for (const event of events) {
